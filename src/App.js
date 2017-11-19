e86047e2577cd09c65773f521dfe64c9d4e08b4d
jiangyu3 <jiangyu3@staff.sina.com.cn>
2017-11-19 21:36:52 +0800

    add tinymce/tinymce-light-skin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import tinymce from 'tinymce';
+import skin from 'tinymce-light-skin';
+import theme from 'tinymce/themes/modern';
+
+skin.use();
+console.log(skin.contentStyle);
+// import skin from 'tinymce/skins/lightgray/skin.min.css';
+// import mobile from 'tinymce/skins/lightgray/skin.mobile.min.css';
 
 class App extends Component {
   static field = 'app';
   render() {
     return (
       <div className="App">
-        {App.field}
+        <textarea defaultValue={App.field}>
+        </textarea>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -17,6 +26,27 @@ class App extends Component {
         </p>
       </div>
     );
+  }
+  componentDidMount() {
+    tinymce.init({
+        selector: 'textarea',
+        height: 500,
+        theme: 'modern',
+        plugins: 'print preview fullpage powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount tinymcespellchecker a11ychecker imagetools mediaembed  linkchecker contextmenu colorpicker textpattern help',
+        toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
+        image_advtab: true,
+        templates: [
+          { title: 'Test template 1', content: 'Test 1' },
+          { title: 'Test template 2', content: 'Test 2' }
+        ],
+        content_style: skin.contentStyle,
+      mobile: {
+        theme: 'mobile',
+        plugins: [ 'autosave', 'lists', 'autolink' ],
+        toolbar: [ 'undo', 'bold', 'italic', 'styleselect' ]
+      }
+    });
+    
   }
 }
 
